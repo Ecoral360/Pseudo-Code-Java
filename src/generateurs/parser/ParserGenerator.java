671c08c84dbd79874a82bec465719cf1384b2833
Ecoral360 <ecomath360@gmail.com>
2020-10-28 14:14:54 -0400

    commit on 'master'

--- src/generateurs/parser/ParserGenerator.java
+++ src/generateurs/parser/ParserGenerator.java
@@ -1,5 +1,31 @@
 package generateurs.parser;
 
+import java.util.Hashtable;
+import java.util.List;
+
+import ast.Ast;
+import tokens.Token;
+
 public class ParserGenerator {
-    
+    Hashtable<String, Ast> programmes = new Hashtable<>();
+    Hashtable<String, Ast> expressions = new Hashtable<>();
+
+
+    public ParserGenerator(){
+
+    }
+
+    protected void ajouterProgramme(String nom, Ast fonction){
+        this.programmes.put(nom, fonction);
+    }
+
+    protected void ajouterExpression(String nom, Ast fonction){
+        this.expressions.put(nom, fonction);
+    }
+
+
+    protected void parse(List<Token> listToken){
+        
+    }
+
 }
